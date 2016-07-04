30550f33666878e37d614aca8edf6929792762c9
likelion_hj <gmlwh0716@naver.com>
2016-07-04 09:32:07 +0000

    초안 완료

---
+++ app/controllers/board_controller.rb
@@ -0,0 +1,26 @@
+class BoardController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def create
+    @post = Post.new(title: params[:title], content: params[:content])
+    @post.save
+    redirect_to :back
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+  
+  def comment_create
+    @comment = Comment.new(post_id: params[:post_id], content: params[:content])
+    @comment.save
+    redirect_to :back
+  end
+end
